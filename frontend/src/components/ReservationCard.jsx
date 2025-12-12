8a4fec8a3804e8577e380e7defc4e87d01564c10
Oscar Gordillo Corral <oscar.gordillo@issec.internal>
2025-12-12 11:41:22 +0000

    Hero añadido en la pestaña de producto

--- frontend/src/components/ReservationCard.jsx
+++ frontend/src/components/ReservationCard.jsx
@@ -4,23 +4,32 @@ function ReservationCard() {
 
     return (
         <div className="w-72 m-7 ">
-            <Link to="/producto">
-                <div className="card bg-base-100  shadow-xl">
-                    <figure>
-                        <img
-                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                            alt="Shoes"
-                            width={100}/>
-                    </figure>
-                    <p>Nombre de la cita</p>
-                    <div className="card-body">
-                        <div className="grid grid-cols-2 gap-1">
-                            <p>Precio</p>
-                            <p>Dificultad</p>
-                        </div>
+            <div className="card bg-base-100  shadow-xl" onClick={() => document.getElementById('my_modal_1').showModal()}>
+                <figure>
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes"
+                        width={100} />
+                </figure>
+                <p>Nombre de la cita</p>
+                <div className="card-body">
+                    <div className="grid grid-cols-2 gap-1">
+                        <p>Precio</p>
+                        <p>Dificultad</p>
                     </div>
                 </div>
-            </Link>
+            </div>
+            <dialog id="my_modal_1" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Pedir cita</h3>
+                    <p className="py-4">Debería mostrarse esto cada vez que le doy a una carta de cita, cada modal debería ser distinto ya que el id contendría el id de la cita</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 }
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,6 +3,7 @@ import '../App.css'
 import { useNavigate } from 'react-router'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import ProductoInfo from '../components/Product'
 
 function Producto() {
   //Producto es cuando le doy adentro de una carta de producto
@@ -12,7 +13,7 @@ function Producto() {
   return (
     <>
       <Header />
-      
+      <ProductoInfo/>
       <Footer />
     </>
   )
