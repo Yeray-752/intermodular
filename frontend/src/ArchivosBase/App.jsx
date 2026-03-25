bb10d784251722014bf81250b4c0db1340fef1bb
Yeray <yeraycarrioncero21@gmail.com>
2026-03-25 08:58:57 +0000

    prueba de importacion de colores

--- frontend/src/ArchivosBase/App.jsx
+++ frontend/src/ArchivosBase/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n'; // importa tu configuración
+import '../style/App.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
