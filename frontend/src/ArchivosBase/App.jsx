03cb9f472cfc3e0d787f1d2823d200e7874d2f39
Yeray <yeraycarrioncero21@gmail.com>
2026-03-25 14:06:14 +0000

    collores arreglados...

--- frontend/src/ArchivosBase/App.jsx
+++ frontend/src/ArchivosBase/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n'; // importa tu configuración
 import '../style/App.css'
+import '../style/index.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
